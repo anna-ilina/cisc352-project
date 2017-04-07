1d45b468af5c5b8d8afc5b7a030be49b65b50c07
Anna Ilina <ilina.anna@queensu.ca>
2017-04-07 01:11:57 -0400

    perceptron complete

--- orPerceptron/orPerceptron.py
+++ orPerceptron/orPerceptron.py
@@ -1,23 +1,8 @@
-# ##
-# You can and should use the Step activation function for the OR perceptron.
-# Multilayer neural networks are trained using backpropagation. A requirement
-# for backpropagation is a differentiable activation function due to its use of
-# gradient descent to update the weights. The step function is not differentiable
-# at x=0, and its derivative is 0 elsewhere; thus, gradient descent won't be able
-# to update the weights and backpropagation will fail. The sigmoid function does
-# not have this problem, so use it for the multilayer neural network.
-#
-# step activation function
-#
-# JUSTIN:
-# perceptron is supervised, how will we decide that our output is expected, without
-# any sort of target output given? Should we create our own expected array by
-# examining if '1' is in the input?
-#
-# PROF:
-# The picture I included in the assignment didn't include a bias because I didn't
-# want you to use one for this, or Bias = 0, in other words.
-# You will need to train your perceptron.
+# CISC352 Artificial Intelligence
+# Perceptron OR rule
+# Assignment 4
+# due April 4th 2017
+# by Anna Ilina (10150979) and Jake Pittis (10148335)
 
 import random
 
@@ -36,6 +21,7 @@ def createTestDataFile(filename, trainingData):
       f.write("]\n\n")
   return
 
+
 def createTrainingData():
   numTrainingSets = 2**NUM_INPUTS
   trainingData = [None] * (numTrainingSets) # each input has 2 options: 1 or 0
@@ -49,6 +35,7 @@ def createTrainingData():
           trainingData[i] = testcase
           i += 1
   return trainingData
+
 
 def readDataFromFile(filename):
   with open(filename, 'r') as f:
@@ -65,6 +52,7 @@ def readDataFromFile(filename):
       inputCases.append(inputSet)
   return inputCases
 
+
 def writeSolutionToFile(filename, solutionSet):
   with open(filename, 'w') as f:
     for solution in solutionSet:
@@ -73,53 +61,68 @@ def writeSolutionToFile(filename, solutionSet):
       f.write("]\n\n")
   return
 
+
 def trainPerceptron(trainingData):
 
-  # initialize weights randomly
+  # Initialize weights randomly
   weights = [None] * NUM_INPUTS
   for i in range(NUM_INPUTS):
     weights[i] = random.uniform(-1, 1)
 
-  print ("initially, weights are " + str(weights))
-
+  # Loop through the training dataset and adjust weights until the you get through a run through the entire dataset with no incorrect classifications
   trainingComplete = False
-
   while trainingComplete == False:
-    trainingComplete = True # assume weights are good unless find a case where trained guess different from target
+    # Assume weights are good unless find a case where trained guess different from target
+    trainingComplete = True
 
     for dataSet in trainingData:
-      target = dataSet[NUM_INPUTS] # last value is the correct 'OR' value
+      target = dataSet[NUM_INPUTS] # Last value is the correct 'OR' value
 
-      # compute answer using weights
+      # Compute answer (to use for Step Activation) using weights
       computed = 0
       for i in range(NUM_INPUTS):
         computed += weights[i] * dataSet[i]
 
-      # compute error
+      # Compute error
       error = float(target) - computed
 
-      # classify as 1 or 0 based on computed answer
+      # Step activation: classify as 1 or 0 based on computed answer
       if computed > 0:
         computedAnswer = 1
       else:
         computedAnswer = 0
 
+      # If classification incorrect, adjust weights
       if computedAnswer != target:
         trainingComplete = False
-        # adjust weights accordingly
         for i in range(NUM_INPUTS):
           weights[i] += LEARNING_RATE * dataSet[i] * error
 
   print "done training"
+  return weights
 
-def classifyData():
-  pass
+def classifyData(testCases, weights):
+  solution = [None] * len(testCases)
+
+  for j in range(len(testCases)):
+
+    # Compute answer using weights (to be used for step activation)
+    computed = 0
+    for i in range(NUM_INPUTS):
+      computed += weights[i] * testCases[j][i]
+
+    # Step activation: classify as 1 or 0 based on computed answer
+    if computed > 0:
+      solution[j] = 1
+    else:
+      solution[j] = 0
+
+  return solution
 
 
 def main():
 
   testFile = "testFile.txt"
-
   inFile = "in.txt"
   outFile = "out.txt"
 
@@ -128,21 +131,21 @@ def main():
 
   print(trainingData)
 
-
-  #createTestDataFile(testFile, trainingData)
-  #testCases = readDataFromFile(testFile)
-  #print testCases
-
-
   #train perceptrons to get weights
-  trainPerceptron(trainingData)
+  weights = trainPerceptron(trainingData)
 
-  #use weights to classify input file
+  # test that it's working
+  # createTestDataFile(testFile, trainingData)
+  # testCases = readDataFromFile(testFile)
+  # solution = classifyData(testCases, weights)
+  # print testCases
+  # print solution
 
-  #write classifications to file
+  testCases = readDataFromFile(inFile)
+  solution = classifyData(testCases, weights)
+  writeSolutionToFile("out.txt", solution)
 
-  print ("curry")
-  pass
+  return
 
 if __name__ == "__main__":
   main()
